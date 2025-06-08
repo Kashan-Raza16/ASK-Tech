26b7f5f31c6c01685c308884d003396d1da176ba
Suraj Kumar <surajkumar620103@gmail.com>
2025-06-08 22:55:29 +0530

    Add HowWrok component and integrate it into Home; remove unused Home2 div from HeroSection

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../component/Header/Header.jsx'
 import HeroSection from './HeroSection.jsx'
+import HowWrok from './HowWrok.jsx'
 
 const Home = () => {
   return (
     <>
       <Header />
       <HeroSection />
+      <HowWrok />
     </>
   )
 }
