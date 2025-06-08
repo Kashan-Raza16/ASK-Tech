bb2648200787adb59095f0e1271e1284824c137d
Suraj Kumar <surajkumar620103@gmail.com>
2025-06-08 23:05:57 +0530

    Add js-yaml and nanoid packages with necessary files and documentation
    - Introduced js-yaml package with license, README, index file, and package.json.
    - Added nanoid package with license, README, index files for both CommonJS and ES modules, and package.json.
    - Updated Home component to include overflow-hidden for better layout management.

--- src/Pages/Home/HowWrok.jsx
+++ src/Pages/Home/HowWrok.jsx
@@ -3,9 +3,9 @@ import BgCircle from '../../assets/img/bg-circle.png'
 
 const HowWrok = () => {
   return (
-    <div className='bg-[#5B45CC] h-screen w-full flex items-center justify-center'> {/* Center container */}
+    <div className='bg-[#5B45CC] h-screen w-full flex items-center justify-center overflow-hidden'> 
         <div className="bg-img relative overflow-hidden flex left-[30%] justify-center">
-            <img src={BgCircle} alt="" className='h-[150vh] object-contain opacity-20' /> {/* Center image and set height */}
+            <img src={BgCircle} alt="" className='h-[150vh] object-contain opacity-20' /> 
         </div>
     </div>
   )
