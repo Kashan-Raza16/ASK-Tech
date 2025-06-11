b271047a6abd5814d2bc4403c1b1c055434bce66
Suraj Kumar <surajkumar620103@gmail.com>
2025-06-11 10:25:17 +0530

    Refactor Card component imports for consistency; adjust HowWrok layout padding for better alignment; enhance swiper pagination styles

--- src/Pages/Home/Card.jsx
+++ src/Pages/Home/Card.jsx
@@ -1,21 +1,17 @@
-import React from 'react'
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-import '../../../style.css';
+import React from "react";
+import "swiper/css";
+import "../../../style.css";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 const Card = () => {
   return (
-    <div className='pl-28 pr-28 h-screen bg-white-900'>
-      
+    <div className="pl-28 pr-28 h-screen bg-white-900">
       <Swiper
         slidesPerView={3}
         spaceBetween={30}
-         loop={true}
+        loop={true}
         autoplay={{
           delay: 2500,
           disableOnInteraction: false,
@@ -26,20 +22,15 @@ const Card = () => {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper h-screen pb-5 "
       >
-        <SwiperSlide className='text-gray-800'>slide1</SwiperSlide>
-        <SwiperSlide className='text-gray-800'>Slide 2</SwiperSlide>
-        <SwiperSlide className='text-gray-800'>Slide 3</SwiperSlide>
-        <SwiperSlide className='text-gray-800'>Slide 4</SwiperSlide>
-        <SwiperSlide className='text-gray-800'>Slide 5</SwiperSlide>
-        <SwiperSlide className='text-gray-800'>Slide 6</SwiperSlide>
-        <SwiperSlide className='text-gray-800'>Slide 7</SwiperSlide>
-        <SwiperSlide className='text-gray-800'>Slide 8</SwiperSlide>
-        <SwiperSlide className='text-gray-800'>Slide 9</SwiperSlide>
+        <SwiperSlide className="text-gray-800">slide1</SwiperSlide>
+        <SwiperSlide className="text-gray-800">Slide 2</SwiperSlide>
+        <SwiperSlide className="text-gray-800">Slide 3</SwiperSlide>
+        <SwiperSlide className="text-gray-800">Slide 4</SwiperSlide>
+        <SwiperSlide className="text-gray-800">Slide 5</SwiperSlide>
+        <SwiperSlide className="text-gray-800">Slide 6</SwiperSlide>
       </Swiper>
-      
     </div>
+  );
+};
 
-  )
-}
-
-export default Card
+export default Card;
