7d27a97df287909f95068adc08ad931681e0157f
Suraj Kumar <surajkumar620103@gmail.com>
2025-06-15 06:33:09 +0530

    .env Updated

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -8,8 +8,8 @@ import Layout from "../Layout.jsx";
 import About from "../Pages/About/About.jsx";
 import Service from "../Pages/Service/Service.jsx";
 import Product from "../Pages/Products/Products.jsx";
+import Projects from "../Pages/Projects/Projects.jsx";
 import StudentSection from "../Pages/StudentSection/StudentSection.jsx";
-import { Projects } from "../Pages/Projects/Projects.jsx";
 
 
 const router = createBrowserRouter(
