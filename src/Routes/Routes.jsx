87dcf800fe68d73d34db86441fc8e67744b33d34
Kashan-Raza16 <kashanahmad16@gmail.com>
2025-06-18 20:57:52 +0530

    update Conflict

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -6,9 +6,9 @@ import {
 import Home from "../Pages/Home/Home.jsx";
 import Layout from "../Layout.jsx";
 import About from "../Pages/About/About.jsx";
-// import Service from "../Pages/Service/Service.jsx";
-import Product from "../Pages/Products/Products.jsx";
-import Projects from "../Pages/Projects/Projects.jsx";
+import Service from "../Pages/Service/index.jsx";
+// import Product from "../Pages/Products/Products.jsx";
+// import Projects from "../Pages/Projects/Projects.jsx";
 import StudentSection from "../Pages/Student/index.jsx";
 
 
@@ -17,17 +17,17 @@ const router = createBrowserRouter(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
       <Route path="About" element={<About />} />
-      {/* <Route path="Service" element={<Service />} /> */}
-      <Route path="Product" element={<Product />} />
+      <Route path="Service" element={<Service />} />
+      {/* <Route path="Product" element={<Product />} /> */}
       <Route path="student-section" element={<StudentSection />} />
-      <Route 
+      {/* <Route 
       loader={() => {
         // Simulating a data fetch for the student section
         return { projectID: "12345" }; // Example project ID
       }
       }
       path="project/:projectID" 
-      element={<Projects />} />
+      element={<Projects />} /> */}
     </Route>
   )
 );
