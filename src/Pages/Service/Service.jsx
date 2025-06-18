db8c01a1f42b057df626635aec6c147618aa99db
Suraj Kumar <surajkumar620103@gmail.com>
2025-06-18 20:42:24 +0530

    refactor(Service): remove unused Service component and update routes

--- src/Pages/Service/Service.jsx
+++ src/Pages/Service/Service.jsx
@@ -1,18 +1,13 @@
-import React from 'react'
-import Page1 from './Page1'
+// import React from 'react'
+// import Page1 from './Page1'
 
-const Service = () => {
-  return (
-   <>
-   <Page1/>
-   
-   
-   
-   
-   
-   
-   </>
-  )
-}
+// const Service = () => {
+//   return (
+//    <>
+//    <Page1/>
+ 
+//    </>
+//   )
+// }
 
-export default Service
+// export default Service
